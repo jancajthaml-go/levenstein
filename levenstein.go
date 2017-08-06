6de58333ba82399d49f14938f7ad57dad6be9576
Jan Cajthaml <jan.cajthaml@gmail.com>
2017-08-06 15:34:34 +0200

    nested conditions flattening

--- levenstein.go
+++ levenstein.go
@@ -1,8 +1,15 @@
 package main
 
 func LevensteinDistance(a, b []byte) int {
+	if len(a) > len(b) {
+		return distance(a, b)
+	} else {
+		return distance(b, a)
+	}
+}
+
+func distance(a, b []byte) int {
 	var (
-		f_curr int
 		f_prev int
 		i1     int   = 0
 		l1     int   = len(b) + 1
@@ -16,32 +23,34 @@ func LevensteinDistance(a, b []byte) int {
 s1:
 	f[i1] = i1
 	i1++
-	if i1 == l1 {
-		goto s2
+	if i1 != l1 {
+		goto s1
 	}
-	goto s1
 s2:
-	ca := a[i2]
 	f_prev = f[0]
 	f[0]++
 s3:
 	cb := b[i3]
 	i3++
-	if f[i3] <= f[i3-1] {
-		f_curr = f[i3] + 1 // delete
+
+	if f[i3] <= f[i3-1] && cb != a[i2] && f[i3] > f_prev {
+		f_prev, f[i3] = f[i3], f_prev+1
+	} else if f[i3] <= f[i3-1] && cb != a[i2] {
+		f_prev, f[i3] = f[i3], f[i3]+1
+	} else if f[i3] <= f[i3-1] && f[i3]+1 > f_prev {
+		f_prev, f[i3] = f[i3], f_prev
+	} else if f[i3] <= f[i3-1] {
+		f_prev, f[i3] = f[i3], f[i3]+1
+	} else if cb != a[i2] && f[i3-1] > f_prev {
+		f_prev, f[i3] = f[i3], f_prev+1
+	} else if cb != a[i2] {
+		f_prev, f[i3] = f[i3], f[i3-1]+1
+	} else if f[i3-1]+1 > f_prev {
+		f_prev, f[i3] = f[i3], f_prev
 	} else {
-		f_curr = f[i3-1] + 1 // insert
+		f_prev, f[i3] = f[i3], f[i3-1]+1
 	}
-	if cb != ca {
-		if f_curr > f_prev+1 { // change
-			f_curr = f_prev + 1
-		}
-	} else {
-		if f_curr > f_prev { // matched
-			f_curr = f_prev
-		}
-	}
-	f_prev, f[i3] = f[i3], f_curr
+
 	if i3 != l3 {
 		goto s3
 	}
