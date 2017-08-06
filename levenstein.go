7fdb2471e0cb5d120faf18733f3b78945b01ae2e
Jan Cajthaml <jan.cajthaml@gmail.com>
2017-08-06 16:08:35 +0200

    added unit tests

--- levenstein.go
+++ levenstein.go
@@ -1,7 +1,11 @@
 package main
 
 func LevensteinDistance(a, b []byte) int {
-	if len(a) > len(b) {
+	if len(a) == 0 {
+		return len(b)
+	} else if len(b) == 0 {
+		return len(a)
+	} else if len(a) > len(b) {
 		return distance(a, b)
 	} else {
 		return distance(b, a)
@@ -10,54 +14,54 @@ func LevensteinDistance(a, b []byte) int {
 
 func distance(a, b []byte) int {
 	var (
-		f_prev int
-		i1     int   = 0
-		l1     int   = len(b) + 1
-		i2     int   = 0
-		l2     int   = len(a)
-		i3     int   = 0
-		l3     int   = len(b)
-		f      []int = make([]int, len(b)+1)
+		prev int
+		i1   int   = 0
+		l1   int   = len(b) + 1
+		i2   int   = 0
+		l2   int   = len(a)
+		i3   int   = 0
+		l3   int   = len(b)
+		f    []int = make([]int, len(b)+1)
 	)
 
 s1:
 	f[i1] = i1
 	i1++
-	if i1 != l1 {
+	if i1 < l1 {
 		goto s1
 	}
-s2:
-	f_prev = f[0]
+outer:
+	prev = f[0]
 	f[0]++
-s3:
-	cb := b[i3]
-	i3++
-
-	if f[i3] <= f[i3-1] && cb != a[i2] && f[i3] > f_prev {
-		f_prev, f[i3] = f[i3], f_prev+1
-	} else if f[i3] <= f[i3-1] && cb != a[i2] {
-		f_prev, f[i3] = f[i3], f[i3]+1
-	} else if f[i3] <= f[i3-1] && f[i3]+1 > f_prev {
-		f_prev, f[i3] = f[i3], f_prev
-	} else if f[i3] <= f[i3-1] {
-		f_prev, f[i3] = f[i3], f[i3]+1
-	} else if cb != a[i2] && f[i3-1] > f_prev {
-		f_prev, f[i3] = f[i3], f_prev+1
-	} else if cb != a[i2] {
-		f_prev, f[i3] = f[i3], f[i3-1]+1
-	} else if f[i3-1]+1 > f_prev {
-		f_prev, f[i3] = f[i3], f_prev
+inner:
+	if f[i3+1] <= f[i3] && b[i3] != a[i2] && f[i3+1] > prev {
+		prev, f[i3+1] = f[i3+1], prev+1
+	} else if f[i3+1] <= f[i3] && b[i3] != a[i2] {
+		prev, f[i3+1] = f[i3+1], f[i3+1]+1
+	} else if f[i3+1] <= f[i3] && f[i3+1]+1 > prev {
+		prev, f[i3+1] = f[i3+1], prev
+	} else if f[i3+1] <= f[i3] {
+		prev, f[i3+1] = f[i3+1], f[i3+1]+1
+	} else if b[i3] != a[i2] && f[i3] > prev {
+		prev, f[i3+1] = f[i3+1], prev+1
+	} else if b[i3] != a[i2] {
+		// SAME as 50
+		prev, f[i3+1] = f[i3+1], f[i3]+1
+	} else if f[i3]+1 > prev {
+		prev, f[i3+1] = f[i3+1], prev
 	} else {
-		f_prev, f[i3] = f[i3], f[i3-1]+1
+		// SAME as 44
+		prev, f[i3+1] = f[i3+1], f[i3]+1
 	}
 
+	i3++
 	if i3 != l3 {
-		goto s3
+		goto inner
 	}
 	i3 = 0
 	i2++
 	if i2 != l2 {
-		goto s2
+		goto outer
 	}
 
 	return f[len(f)-1]
--- levenstein_test.go
+++ levenstein_test.go
@@ -21,3 +21,46 @@ func BenchmarkLevensteinSerial(b *testing.B) {
 		LevensteinDistance(left, right)
 	}
 }
+
+func TestDistanceOnEmptyStrings(t *testing.T) {
+	expectDistance := func(a string, b string, c int) {
+		d := LevensteinDistance([]byte(a), []byte(b))
+		if d != c {
+			t.Errorf("a: `" + a + "`, b: `" + b + "` expected " + string(c+48) + " , got " + string(d+48))
+		}
+	}
+
+	expectDistance("", "", 0)
+	expectDistance("a", "a", 0)
+	expectDistance("abc", "abc", 0)
+
+	expectDistance("a", "", 1)
+	expectDistance("", "a", 1)
+	expectDistance("abc", "", 3)
+	expectDistance("", "abc", 3)
+
+	// inserts only
+	expectDistance("a", "ab", 1)
+	expectDistance("b", "ba", 1)
+	expectDistance("ac", "abc", 1)
+	expectDistance("abcdefg", "xabxcdxxefxgx", 6)
+
+	// deletes only
+	expectDistance("ab", "a", 1)
+	expectDistance("ab", "b", 1)
+	expectDistance("abc", "ac", 1)
+	expectDistance("xabxcdxxefxgx", "abcdefg", 6)
+
+	// swaps only
+	expectDistance("a", "b", 1)
+	expectDistance("ab", "ac", 1)
+	expectDistance("ac", "bc", 1)
+	expectDistance("abc", "axc", 1)
+	expectDistance("xabxcdxxefxgx", "1ab2cd34ef5g6", 6)
+
+	// combination of insert + delete + swap
+	expectDistance("example", "samples", 3)
+	expectDistance("sturgeon", "urgently", 6)
+	expectDistance("levenshtein", "frankenstein", 6)
+	expectDistance("distance", "difference", 5)
+}
